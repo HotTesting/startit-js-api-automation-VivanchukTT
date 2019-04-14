2f6e8aef8fea9b5c121292a21c64b17157829af4
VivanchukTT <v.ivanchuk@trip-top.com>
2019-04-14 18:34:16 +0300

    home-task

---
+++ constructor/request.ts
@@ -0,0 +1,68 @@
+import * as request from "request-promise-native";
+import { URL } from "url";
+
+export class Request {
+    client = request;
+    options: request.OptionsWithUri;
+
+    constructor(absoluteURL: string) {
+        this.options = {
+            uri: absoluteURL,
+            method: "GET"
+        };
+        this.client = request.defaults({
+            json: true, 
+            time: true,
+            resolveWithFullResponse: true,
+            followAllRedirects: true
+        });
+    }
+    public method(method: "POST" | "GET" | "DELETE"): Request {
+        this.options.method = method;
+        return this;
+    }
+    public queryParameters(queryParameters: Object): Request {
+        this.options.qs = queryParameters;
+        return this;
+    }
+    public body(body){
+        this.options.body = body;
+        return this;
+    }
+    public headers(headers: Object) {
+        this.options.headers = headers;
+        return this;
+    }
+    public cookies(cookiesJar) {
+        this.options.jar = cookiesJar;
+        return this;
+    }
+    public async send(): Promise<request.FullResponse> {
+        let response = this.client(this.options);
+        this.logResponse(response);
+        return response;
+    }
+    private async logResponse(responsePromise: request.RequestPromise<any>) {
+        try {
+            let response = await responsePromise;
+            console.log(
+                `${this.options.method}:${response.statusCode}: ${
+                    this.options.uri
+                } took: ${response.timingPhases.total.toFixed()} ms`
+            );
+            console.log(
+                `RESPONSE BODY: ${JSON.stringify(response.body, null, 2)}`
+            );
+        } catch (error) {
+            if (error.response) {
+                console.warn(
+                    `${this.options.method}:${error.response.statusCode}: ${
+                        this.options.uri
+                    } took: ${error.response.timingPhases.total} ms`
+                );
+            } else {
+                console.warn(error.message || error);
+            }
+        }
+    }
+}
--- tests/test.ts
+++ tests/test.ts
@@ -1,18 +1,172 @@
 import * as request from "request-promise-native";
+import {Request} from "../constructor/request";
+import * as faker from "faker";
+import {expect} from "chai";
+const baseUrl = "http://ip-5236.sunline.net.ua:30020";
+const adminEmail = "test@test.com";
+const pass = "123456";
 
-describe("Search", function() {
-    it("should be successful", async function() {
-        let resp = await request.post(
-            "http://ip-5236.sunline.net.ua:30020/users/login",
-            {
-                json: true,
-                body: {
-                    email: "wekan_superadmin@ip-5236.sunline.net.ua",
-                    password: "wekan_superadmin@ip-5236.sunline.net.ua"
-                }
-            }
-        );
-        console.log("Login successful!", resp);
-        console.log("You are set up!");
+
+describe("Set of tests", function() {
+    it("Should get the User information", async function() {
+        let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+            .method('POST')
+            .body({email: adminEmail,
+               password: pass
+                })
+            .send();
+        console.log("Login successful!");
+        let getUsersInfoResp = await new Request(`${baseUrl}/api/user`)
+            .method("GET")
+            .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+            .send();
+        console.log("User info returned!");
+        expect(getUsersInfoResp.body).to.have.property(`username`);
+        expect(getUsersInfoResp.body).to.not.have.property(`Forbidden`);
     });
-});
+
+    it("NEGATIVE: Should return Error", async function() {
+        let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+            .method('POST')
+            .body({email: adminEmail,password: pass})
+            .send();
+        let getUsersInfoResp = await new Request(`${baseUrl}/api/user`)
+            .method("GET")
+            .headers({Authorization: "InvalidToken"})
+            .send();
+            try {
+                expect(getUsersInfoResp).to.have.property(`username`);
+                console.log("User info returned!");
+            }
+            catch(err){
+                console.log("User info NOT returned!");  
+            }
+    });
+
+    it("Should create && delete new User", async function() {
+        let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+            .method('POST')
+            .body({email: adminEmail,
+                   password: pass
+                })
+            .send();
+        console.log("Login successful!");
+        //create a New User
+        let createUserResponse = await new Request(`${baseUrl}/api/users`)
+            .method("POST")
+            .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+            .body({username: "MyUserForDeletion",
+                   email: faker.internet.email(undefined, undefined, `ip-5236.sunline.net.ua`),
+                   password: faker.internet.password(),
+                   fromAdmin: true                          
+                })
+            .send();
+        console.log("User created!");
+        const newUsersID = `${createUserResponse.body._id}`
+        //Delete the User
+        let deleteUserResponse = await new Request(`${baseUrl}/api/users/${newUsersID}`)
+            .method("DELETE")
+            .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+            .send();
+        console.log("User deleted!");
+        expect(deleteUserResponse.body).to.have.property(`_id`);
+        expect(deleteUserResponse.body).to.not.have.property(`Forbidden`);
+        });  
+
+    it("NEGATIVE: Should fail deletion of the new User", async function() {
+            let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+                .method('POST')
+                .body({email: adminEmail,password: pass})
+                .send();
+            console.log("Login successful!");
+            //create a New User
+            let createUserResponse = await new Request(`${baseUrl}/api/users`)
+                .method("POST")
+                .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+                .body({username: faker.internet.userName(),
+                       email: faker.internet.email(undefined, undefined, `ip-5236.sunline.net.ua`),
+                       password: faker.internet.password(),
+                       fromAdmin: true})
+                .send();
+            const newUsersID = `${createUserResponse.body._id}`
+            //Delete the User -- incorrect ID
+            let deleteUserResponse = await new Request(`${baseUrl}/api/users/incorrectID123456`)
+                .method("DELETE")
+                .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+                .send();
+            try {
+            expect(deleteUserResponse.body).to.have.property(`_id`);
+            expect(deleteUserResponse.body).to.not.have.property(`Forbidden`);
+            console.log("User deleted!");
+            }
+            catch(err){
+            console.log("User NOT deleted!");
+            }
+            });
+          
+    it("Should register new User", async function() {
+        let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+            .method('POST')
+            .body({email: adminEmail,
+                   password: pass
+                })
+            .send();
+        console.log("Login successful!");
+        let registerUserResponse = await new Request(`${baseUrl}/users/register`)
+        .method("POST")
+        .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+        .body({username: faker.internet.userName(),
+               email: faker.internet.email(undefined, undefined, `ip-5236.sunline.net.ua`),
+               password: faker.internet.password(),                           
+              })
+        .send(); 
+            //get the Information about the new User
+        const newUsersID = `${registerUserResponse.body.id}`
+        let getNewUserInfo = await new Request(`${baseUrl}/api/users/${newUsersID}`)
+        .method("GET")
+        .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})        
+        .send();
+        expect(registerUserResponse.body).to.have.property(`id`);  
+        expect(registerUserResponse.body).to.not.have.property(`Forbidden`);
+    }); 
+
+    it("Get a Users list", async function() {
+        let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+            .method('POST')
+            .body({email: adminEmail,
+                   password: pass
+                })
+            .send();
+        console.log("Login successful!");
+        let getUsersListResp = await new Request(`${baseUrl}/api/users`)
+        .method("GET")
+        .headers({Authorization: `Bearer ${adminLoginResp.body.token}`})
+        .send();
+        console.log("Users list returned!");
+        expect(getUsersListResp.body).not.empty;
+        console.log("User info returned!", getUsersListResp);
+        });
+
+    it("NEGATIVE: Get a Users list", async function() {
+        let adminLoginResp = await new Request(`${baseUrl}/users/login`)
+                .method('POST')
+                .body({email: adminEmail,
+                       password: pass
+                    })
+                .send();
+            console.log("Login successful!");
+            let getUsersListResp = await new Request(`${baseUrl}/api/users`)
+            .method("GET")
+            .headers({Authorization: "IncorrectToken"})
+            .send();
+         try{
+            expect(getUsersListResp.body).not.have.property(`error`);
+            console.log("User list returned!");
+            }
+         catch(err){
+            console.log("User list NOT returned!");
+            }
+
+            });
+    });
+
